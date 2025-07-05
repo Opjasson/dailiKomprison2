6268422c7121a3aa079dd1de9afab0bbfd51f4b5
Opjasson <azmitheusos333@gmail.com>
2025-07-05 20:02:35 +0700

    frontend : clear page setting

--- dailykomparison2/app/Pages/index.tsx
+++ dailykomparison2/app/Pages/index.tsx
@@ -1,12 +1,8 @@
-import Home from "./Home"
-import Input from "./Input/index"
-import Ranking from "./Ranking"
-import splashScreen from "./SplashScreen"
+import Home from "./Home";
+import Input from "./Input";
+import Ranking from "./Ranking";
+import splashScreen from "./SplashScreen";
+import UpdatePage from "./Update";
+import Register from "./Register";
 
-
-export {
-    Home,
-    Input,
-    Ranking,
-    splashScreen
-}
+export { Home, Input, Ranking, splashScreen, UpdatePage, Register };
--- dailykomparison2/app/Routes/index.tsx
+++ dailykomparison2/app/Routes/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import { StatusBar, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationIndependentTree } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home, Input, Ranking, splashScreen } from "../Pages";
-import inputData from "../Pages/Input";
-import updatePage from "../Pages/Update/updatePage";
+import { Home, Input, Ranking, UpdatePage, splashScreen } from "../Pages";
+
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+
 
 const Route = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="SplashScreen" component={splashScreen} />
             <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Input" component={inputData} />
+            <Stack.Screen name="Input" component={Input} />
             <Stack.Screen name="Rank" component={Ranking} />
-            <Stack.Screen name="Update" component={updatePage} />
+            <Stack.Screen name="Update" component={UpdatePage} />
         </Stack.Navigator>
     );
 };
