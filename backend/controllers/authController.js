e1892fe253f98f5eb7164155b665c1ae7b344d5d
Opjasson <azmitheusos333@gmail.com>
2025-07-05 19:22:08 +0700

    backend : add auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,17 @@
+import Users from "../models/user.js";
+import argon2 from "argon2";
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ where: { email } });
+    if (!user) {
+        return res.status(401).json({
+            message: "Email yang anda masukan salah",
+        });
+    }
+    const isValidPassword = await argon2.verify(user.password, password);
+    if (!isValidPassword) {
+        return res.status(401).json({ message: "password salah" });
+    }
+    res.status(201).json({ message: "Login succesfully", response: user });
+};
