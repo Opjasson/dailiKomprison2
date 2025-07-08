3b9660c4ecaa3281825f6ff57915dd370bf3c070
Opjasson <azmitheusos333@gmail.com>
2025-07-08 08:58:56 +0700

    backend : add route get login and delete logiin

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { deleteLogin, getLogin, login } from "../controllers/authController.js";
 
 const route = express.Router();
 
 route.post("/login", login);
+route.get("/login", getLogin);
+route.delete("/login/:id", deleteLogin);
 
 export default route;
